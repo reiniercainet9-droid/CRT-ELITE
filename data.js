69d6db3e7e4436591c2b00d424ab68f3a6b6a34e
REINIER2615 <reiniercainet9@gmail.com>
2026-09-12 11:57:07 -0300

    Add files via upload

--- data.js
+++ data.js
@@ -3,7 +3,7 @@
    Todo el contenido del plan operativo de Rey.
    ============================================================ */
 
-const APP_VERSION = "7.99";
+const APP_VERSION = "7.100";
 
 /* --- CHECKLIST OPERATIVO (4 bloques) --- */
 const CHECKLIST = [
