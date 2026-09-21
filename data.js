dd08e960f352a666c526b4bcb92b9584dc6eedd9
Apex <reiniercainet9@gmail.com>
2026-09-21 19:02:20 -0300

    Apex v7.208

--- data.js
+++ data.js
@@ -3,7 +3,7 @@
    Todo el contenido del plan operativo de Rey.
    ============================================================ */
 
-const APP_VERSION = "7.207";
+const APP_VERSION = "7.208";
 
 /* --- CHECKLIST OPERATIVO (4 bloques) --- */
 const CHECKLIST = [
